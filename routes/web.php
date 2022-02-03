79ecb2e301687b8506f8674431a1f4f58540b6a6
Asadbekinha <fayzulloevasadbek@gmail.com>
2022-02-03 12:05:37 +0500

    fix login

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
-    Route::get('login', [VoyagerAuthController::class, 'login'])->name('voyager.login');
+//    Route::get('login', [VoyagerAuthController::class, 'login'])->name('voyager.login');
 });
