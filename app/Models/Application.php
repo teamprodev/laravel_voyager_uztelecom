703ac3587f04018f3363cd72a6e8ee02c08cc8c5
Asadbekinha <fayzulloevasadbek@gmail.com>
2022-02-03 12:45:25 +0500

    [feature] Application steps

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -11,12 +11,20 @@ class Application extends Model
     public function plan(){
         return $this->belongsTo(Plan::class);
     }
-    public function scopeUser(){
-//        $user = auth()->user();
-//        switch ($user->role_id) {
-//            case 1: {
-//
-//            } break;
-//        }
+    public function scopeUser($query){
+        $user = auth()->user();
+        switch ($user->role_id) {
+            // APPLICATION CREATOR
+            case 1: {
+                return $query->where('user_id', $user->id);
+            } break;
+            //HEAD OF DEPARTMENT of user who created APPLICATION
+            case 2: {
+                // Get all workers id of his department as array
+                $user_list = User::where('department_id', $user->department_id)->pluck('id')->toArray();
+                return $query->whereIn('user_id', $user_list);
+            } break;
+            
+        }
     }
 }
