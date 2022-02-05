bae3e65945dc6ee120df2218e7eb17855f1cc771
Asadbekinha <fayzulloevasadbek@gmail.com>
2022-02-05 11:16:06 +0500

    menu.blade.php

--- resources/views/site/dashboard/menu.blade.php
+++ resources/views/site/dashboard/menu.blade.php
@@ -1,5 +1,8 @@
 <ul>
     @foreach($items as $menu_item)
-        <li><a href="{{ $menu_item->link() }}">{{ $menu_item->title }}</a></li>
+        <li class="py-2">
+            <i class="fas fa-bookmark text-white mr-1"></i>
+            <a class="text-white hover:text-blue-200" href="{{ $menu_item->link() }}">{{ $menu_item->title }}</a>
+        </li>
     @endforeach
 </ul>
