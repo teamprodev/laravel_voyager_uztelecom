af5a3ce5846f5af8fbf9dc995434410a3fcca67d
asilbek20041207 <asilbek.pardabayev@bk.ru>
2022-02-12 13:26:54 +0500

    table button

--- resources/views/site/applications/index.blade.php
+++ resources/views/site/applications/index.blade.php
@@ -18,6 +18,7 @@
                 <th data-priority="4">Харид режаси (сумма)</th>
                 <th data-priority="6">Махсулотни келтириш учун қўйилган талаб INCOTERMS, (омбордан олиб кетиш/ харидорга етказиб бериш)</th>
                 <th data-priority="8">Дата создания</th>
+                <th></th>
             </tr>
             </thead>
             <tbody>
@@ -30,6 +31,14 @@
                     <td>{{ $application->amount }} {{ $application->currency }}</td>
                     <td>{{ $application->incoterms }}</td>
                     <td>{{ $application->created_at }}</td>
+                    <td class="w-48">
+                        <a href="{{ route('site.applications.edit') }}">
+                            <button type="button" class="inline-block px-6 py-2.5 bg-blue-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-lg transition duration-150 ease-in-out">Edit</button>
+                        </a>
+                        <a href="{{ route('site.applications.show') }}">
+                            <button type="button" class="inline-block px-6 py-2.5 bg-yellow-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-800 active:shadow-lg transition duration-150 ease-in-out">Show</button>
+                        </a>
+                    </td>
                 </tr>
             @endforeach
             </tbody>
