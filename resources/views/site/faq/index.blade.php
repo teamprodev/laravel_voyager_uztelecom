18593f5853c1ea6e45790f706b0f85480d3616f0
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-02-12 15:19:54 +0500

    faq blade

--- resources/views/site/faq/index.blade.php
+++ resources/views/site/faq/index.blade.php
@@ -1,52 +1,18 @@
 @extends('site.layouts.wrapper')
 
 @section('center_content')
-    <div>
-        <h1 class="text-3xl font-semibold">База знаний</h1>
-        <div class="text-lg my-4">
-            Закон Республики Узбекистан «О государственных закупках»
-            <a href="https://lex.uz/uz/docs/5382974"
-               class="text-blue-500 hover:text-red-500"
-            >
-                №684
-            </a>
-            от 22.04.2021г. (вступает силу с 22.07.2021г.)
-        </div>
-        <div class="text-lg my-4">
-            Постановление Президента Республики Узбекистан «О дополнительных мерах по поддержке отечественных производителей»
-            <a href="https://lex.uz/uz/docs/4964633"
-               class="text-blue-500 hover:text-red-500"
-            >
-                №ПП-4812
-            </a>
-            от 21.08.2020 г.
-        </div>
-        <div class="text-lg my-4">
-            Постановление Президента Республики Узбекистан «О мерах по реализации закона Республики Узбекистан «О государственных закупках»
-            <a href="https://lex.uz/uz/docs/5382974"
-               class="text-blue-500 hover:text-red-500"
-            >
-                №ПП-3953
-            </a>
-            от 27.09.2018 г.
-        </div>
-        <div class="text-lg my-4">
-            Указ Президента Республики Узбекистан «О мерах по ускоренному реформированию предприятий с участием государства и приватизации государственных активов»
-            <a href="https://lex.uz/uz/docs/5068824"
-               class="text-blue-500 hover:text-red-500"
-            >
-                №УП-6096
-            </a>
-            от 27.10.2020г.
-        </div>
-        <div class="text-lg my-4">
-            Протокол Кабинета Министров «О приоритетах трансформации акционерной компании «Узбектелеком»
-            <a href="/"
-               class="text-blue-500 hover:text-red-500"
-            >
-                №45
-            </a>
-            от 26.02.2021г.
-        </div>
-    </div>
+   <div class="container mx-auto flex flex-col mt-32 mr-8">
+       <div class="flex justify-between items-center my-3 w-full mx-auto bg-green-100 px-6 py-2">
+           <h1 class="text-lg text-black font-normal">Qanday qilib saytdan foydalanaman?</h1>
+           <a href="/faq/show" class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-5">View</a>
+       </div>
+       <div class="flex justify-between items-center my-3 w-full mx-auto bg-green-100 px-6 py-2">
+           <h1 class="text-lg text-black font-normal">Qanday qilib saytdan foydalanaman?</h1>
+           <a href="/faq/show" class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-5">View</a>
+       </div>
+       <div class="flex justify-between items-center my-3 w-full mx-auto bg-green-100 px-6 py-2">
+           <h1 class="text-lg text-black font-normal">Qanday qilib saytdan foydalanaman?</h1>
+           <a href="/faq/show" class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-5">View</a>
+       </div>
+   </div>
 @endsection
