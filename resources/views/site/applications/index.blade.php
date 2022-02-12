56276aabf180c5e48ddb59a7ec2611565934cb1b
asilbek20041207 <asilbek.pardabayev@bk.ru>
2022-02-12 15:35:10 +0500

    design

--- resources/views/site/applications/index.blade.php
+++ resources/views/site/applications/index.blade.php
@@ -8,30 +8,30 @@
     </a>
     <div class="w-11/12 mx-auto mt-6">
 
-        <table id="table_id" class="display">
+        <table id="table_id">
             <thead>
-            <tr>
-                <th data-priority="1">Id</th>
-                <th data-priority="1">Ташаббускор (буюртмачи номи )</th>
-                <th data-priority="2">Сотиб олинадиган махсулот номи (махсулот, иш, хизмат)</th>
-                <th data-priority="3">Махсулот келишининг муддати</th>
-                <th data-priority="4">Харид режаси (сумма)</th>
-                <th data-priority="6">Махсулотни келтириш учун қўйилган талаб INCOTERMS, (омбордан олиб кетиш/ харидорга етказиб бериш)</th>
-                <th data-priority="8">Дата создания</th>
-                <th></th>
+            <tr class="border ">
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100  text-sm font-semibold text-gray-600" data-priority="1">Id</th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100  text-sm font-semibold text-gray-600" data-priority="1">Ташаббускор (буюртмачи номи )</th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100  text-sm font-semibold text-gray-600 uppercase tracking-wider" data-priority="2">Сотиб олинадиган махсулот номи (махсулот, иш, хизмат)</th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100  text-sm font-semibold text-gray-600 uppercase tracking-wider" data-priority="3">Махсулот келишининг муддати</th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100  text-sm font-semibold text-gray-600 uppercase tracking-wider" data-priority="4">Харид режаси (сумма)</th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100  text-sm font-semibold text-gray-600 uppercase tracking-wider" data-priority="6">Махсулотни келтириш учун қўйилган талаб INCOTERMS, (омбордан олиб кетиш/ харидорга етказиб бериш)</th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100  text-sm font-semibold text-gray-600 uppercase tracking-wider" data-priority="8">Дата создания</th>
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100  text-sm font-semibold text-gray-600 uppercase tracking-wider"></th>
             </tr>
             </thead>
             <tbody>
             @foreach($applications as $application)
-                <tr class="text-center">
-                    <td>{{ $application->id }}</td>
-                    <td>{{ $application->name }}</td>
-                    <td>{{ $application->specification }}</td>
-                    <td>{{ $application->delivery_date }}</td>
-                    <td>{{ $application->amount }} {{ $application->currency }}</td>
-                    <td>{{ $application->incoterms }}</td>
-                    <td>{{ $application->created_at }}</td>
-                    <td class="w-48">
+                <tr class="text-center hover:bg-gray-100">
+                    <td class="border-b">{{ $application->id }}</td>
+                    <td class="border-b">{{ $application->name }}</td>
+                    <td class="border-b">{{ $application->specification }}</td>
+                    <td class="border-b">{{ $application->delivery_date }}</td>
+                    <td class="border-b">{{ $application->amount }} {{ $application->currency }}</td>
+                    <td class="border-b">{{ $application->incoterms }}</td>
+                    <td class="border-b">{{ $application->created_at }}</td>
+                    <td class="w-48 border-b">
                         <a href="{{ route('site.applications.edit') }}">
                             <button type="button" class="inline-block px-6 py-2.5 bg-blue-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-600 hover:shadow-lg focus:bg-blue-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-lg transition duration-150 ease-in-out">Edit</button>
                         </a>
