01857c8ae38fcf95c2aaaf785f927bfe167c854a
Shamshod <62167469+Shamshod-Olimov@users.noreply.github.com>
2022-03-11 11:58:31 +0500

    show blade bug

--- resources/views/site/applications/show.blade.php
+++ resources/views/site/applications/show.blade.php
@@ -7,83 +7,97 @@
         <div class="w-full flex">
             <div class="p-6">
                 {{Aire::textArea('bio','Ташаббускор (буюртмачи номи )')
-                                ->name('initiator')
-                                ->rows(3)
-                                ->cols(40)
-                                ->disabled()
-                            }}
+                    ->name('initiator')
+                    ->value($application->initiator)
+                    ->rows(3)
+                    ->cols(40)
+                    ->disabled()
+                }}
                 {{Aire::textArea('bio','Харид мазмуни (сотиб олиш учун асос)')
                     ->name('purchase_basis')
+                    ->value($application->purchase_basis)
                     ->rows(3)
                     ->cols(40)
                     ->disabled()
                 }}
                 {{Aire::textArea('bio','Асос (харидлар режаси, раҳбарият томонидан билдирги)')
                     ->name('basis')
+                    ->value($application->basis)
                     ->rows(3)
                     ->cols(40)
                     ->disabled()
                 }}
                 {{Aire::textArea('bio','Сотиб олинадиган махсулот номи (махсулот, иш, хизмат)')
                     ->name('name')
+                    ->value($application->name)
                     ->rows(3)
                     ->cols(40)
                     ->disabled()
                 }}
                 {{Aire::textArea('bio','Сотиб олинадиган махсулот тавсифи (техник характери)')
                     ->name('specification')
+                    ->value($application->specification)
                     ->rows(3)
                     ->cols(40)
                     ->disabled()
                 }}
                 {{Aire::dateTimeLocal('bio','Махсулот келишининг муддати')
                     ->name('delivery_date')
+                    ->value($application->delivery_date)
                 }}
                 {{Aire::textArea('bio','Алоҳида талаблар')
                     ->name('separate_requirements')
+                    ->value($application->separate_requirements)
                     ->rows(3)
                     ->cols(40)
                     ->disabled()
                 }}
                 {{Aire::textArea('bio','Махсулотга қўйилган бошқа талаблар (иш, хизмат)')
                     ->name('other_requirements')
+                    ->value($application->other_requirements)
                     ->rows(3)
                     ->cols(40)
                     ->disabled()
                 }}
                 {{Aire::dateTimeLocal('bio','Махсулот сифати учун кафолат муддати (иш, хизмат)')
                     ->name('expire_warranty_date')
+                    ->value($application->expire_warranty_date)
                 }}
                 {{Aire::textArea('bio','Харид режаси (сумма)')
                     ->name('amount')
+                    ->value($application->amount)
                     ->rows(3)
                     ->cols(40)
                     ->disabled()
                 }}
                 {{Aire::textArea('bio','Махсулотни келтириш учун қўйилган талаб INCOTERMS, (омбордан олиб кетиш/ харидорга етказиб бериш)')
                     ->name('incoterms')
+                    ->value($application->incoterms)
                     ->rows(3)
                     ->cols(40)
                     ->disabled()
                 }}
                 {{Aire::textArea('bio','Бюджетни режалаштириш бўлими - харид қилинадиган махсулотни бизнес режада мавжудлиги бўйича маълумот')
                     ->name('budget_planning')
+                    ->value($application->budget_planning)
                     ->rows(3)
                     ->cols(40)
                     ->disabled()
                 }}
                 {{Aire::textArea('bio','Харид килинадиган махсулотни "Харидлар режаси"да мавжудлиги буйича маълумот')
                     ->name('procurement_plan')
+                    ->value($application->procurement_plan)
                     ->rows(3)
                     ->cols(40)
                     ->disabled()
                 }}
                 {{Aire::textArea('bio','Коментарий к заявке')
-                                    ->name('comment')
-                                        ->rows(3)
-                                        ->cols(40)
-                                        ->disabled()
-                            }}
+                    ->name('comment')
+                    ->value($application->comment)
+                    ->rows(3)
+                    ->cols(40)
+                    ->disabled()
+                }}
             </div>
         </div>
     </div>
