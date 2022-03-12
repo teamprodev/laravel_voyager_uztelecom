f22a8805978287ff6b6ec8bbfe337b85924e4508
Botir-Muradullayev <95694305+Botir-Muradullayev@users.noreply.github.com>
2022-03-12 12:06:13 +0300

    Adding "Back" buttons

--- resources/views/site/applications/create.blade.php
+++ resources/views/site/applications/create.blade.php
@@ -1,6 +1,9 @@
 @extends('site.layouts.wrapper')
 
 @section('center_content')
+    <div class="w-full text-right py-4 pr-10">
+        <button class="btn btn-danger" onclick="functionBack()">Назад</button>
+    </div>
     {{ Aire::open()
   ->route('site.applications.store')
   ->enctype("multipart/form-data")
@@ -8,3 +11,10 @@
         @include('site.applications.form')
     {{ Aire::close() }}
 @endsection
+
+<script>
+    function functionBack()
+    {
+        window.history.back();
+    }
+</script>
