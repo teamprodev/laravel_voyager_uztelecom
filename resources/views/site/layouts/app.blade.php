dbc4aa3a3d32023586d820af41f7da90c60c3ea0
arabic0513 <rustamyusupov230@gmail.com>
2022-03-21 23:53:52 +0500

    Yajra Bug Fix

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -133,14 +133,4 @@
 
     });
 </script>
-
-
-<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>
-<!-- Popper JS -->
-<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'></script>
-<!-- Bootstrap JS -->
-<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'></script>
-<!-- Custom Script -->
-<script src="{{asset("assets/js/script.js")}}"></script>
-
 </html>
