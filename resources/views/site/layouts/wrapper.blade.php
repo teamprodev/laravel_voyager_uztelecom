84718eca5115f751d04f1af81f4b883259c4621d
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-03-25 19:14:10 +0500

    front adminLTE bug fix

--- resources/views/site/layouts/wrapper.blade.php
+++ resources/views/site/layouts/wrapper.blade.php
@@ -1,14 +1,5 @@
 @extends('site.layouts.app')
 
-<div class="w-2/12 ml-8"></div>
-
-@section('menu-left')
-    @include('site.dashboard.sidebar')
-@endsection
-@section('top-bar')
-    @include('site.dashboard.navbar')
-@endsection
-
 @section('content')
     @include('site.common.alert')
     @yield('center_content')
