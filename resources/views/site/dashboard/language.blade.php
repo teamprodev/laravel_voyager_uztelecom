77d7bbaefe0c3eba2b07fa16701060b6756788ff
Ozodbek1405 <roziyevozodbek75@gmail.com>
2022-04-02 16:33:21 +0500

    lang style bug fix

--- resources/views/site/dashboard/language.blade.php
+++ resources/views/site/dashboard/language.blade.php
@@ -1,7 +1,7 @@
-<ul>
+<ul class="flex flex-row mt-2">
     @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-        <li>
-            <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+        <li class="mx-1">
+            <a class="hover:text-red-500" rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
                 {{ $properties['native'] }}
             </a>
         </li>
