0d553e466ab522764171c7fd78fde2f1bbb95de5
Shamshod <62167469+Shamshod-Olimov@users.noreply.github.com>
2022-04-05 14:49:10 +0500

    sidebar fixed

--- resources/views/site/dashboard/menu.blade.php
+++ resources/views/site/dashboard/menu.blade.php
@@ -2,7 +2,7 @@
     <ul class="nav nav-treeview">
         <li class="nav-item">
             <a href="{{route('site.applications.index')}}" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
+                <i class="nav-icon fas fa-th-list"></i>
                 <p>
                     Все заявки
                 </p>
@@ -10,13 +10,19 @@
         </li>
         <li class="nav-item">
             <a href="{{route('site.applications.create')}}" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
+                <i class="nav-icon fas fa-plus-square"></i>
                 <p>Создать заявку</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{route('site.faqs.index')}}" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
+                <i class="nav-icon fas fa-file-text"></i>
+                <p>Черновик</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{route('site.faqs.index')}}" class="nav-link">
+                <i class="nav-icon fas fa-comment"></i>
                 <p>База знаний</p>
             </a>
         </li>
--- resources/views/site/dashboard/sidebar.blade.php
+++ resources/views/site/dashboard/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Sidebar -->
-    <div class="sidebar position-fixed">
+    <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <a href="#" class="brand-link">
             <img src="{{asset('images/imagen-my-uztelecom-0big.png')}}" alt="Uztelecom Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
