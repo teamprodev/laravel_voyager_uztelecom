1dff416995a83500bece31d80e9348ccf129e7e4
azikboee <101039980+azikboee@users.noreply.github.com>
2022-04-28 18:10:37 +0500

    bug fix number_edit

--- resources/views/site/applications/number_edit.blade.php
+++ resources/views/site/applications/number_edit.blade.php
@@ -1,15 +1,9 @@
 <div class="pt-6">
     <div class="w-full flex">
         <div class="p-6">
-            <div class="flex items-baseline">
-                <div class="mr-4 pt-2 pb-2 w-50">
                         {{Aire::number('num', "Number Application")
                             ->name('number')
-                            ->rows(3)
-                            ->cols(40)
                              }}
-                </div>
-            </div>
         </div>
     </div>
 
