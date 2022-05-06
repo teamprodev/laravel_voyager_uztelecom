d1b6cfe74a93c67d72028a27a74fdd9ec9765977
arabic0513 <rustamyusupov230@gmail.com>
2022-05-06 15:10:46 +0500

    All role create application to yajra

--- app/Http/Controllers/Site/ApplicationController.php
+++ app/Http/Controllers/Site/ApplicationController.php
@@ -159,7 +159,7 @@ class ApplicationController extends Controller
 
             if($user->hasPermission('Add_Company_Signer') && $user->hasPermission('Add_Branch_Signer'))
             {
-                $query = Application::query()->where('signers','like',"%{$user->role_id}%")->orWhere('performer_role_id', $user->role->id);
+                $query = Application::query()->where('signers','like',"%{$user->role_id}%")->orWhere('performer_role_id', $user->role->id)->orWhere('user_id',auth()->user()->id);
             }
             elseif($user->hasPermission('Company_Leader') && $user->hasPermission('Branch_Leader'))
             {
@@ -171,11 +171,11 @@ class ApplicationController extends Controller
         }
         elseif ($user->hasPermission('Company_Signer') || $user->hasPermission('Add_Company_Signer')||$user->hasPermission('Branch_Signer') || $user->hasPermission('Add_Branch_Signer'))
             {
-            $query = Application::query()->where('signers','like',"%{$user->role_id}%")->orWhere('performer_role_id', $user->role->id);
+            $query = Application::query()->where('signers','like',"%{$user->role_id}%")->orWhere('performer_role_id', $user->role->id)->orWhere('user_id',auth()->user()->id);
         }
         elseif ($user->hasPermission('Company_Performer') || $user->hasPermission('Branch_Performer'))
             {
-                $query = $query->where('performer_role_id', $user->role->id);
+                $query = Application::query()->where('performer_role_id', $user->role->id)->orWhere('user_id',auth()->user()->id);
             }
             elseif($user->hasPermission('Company_Leader'))
             {
