3559daa32b7600e10841330f511dda6f00e8cb7f
arabic0513 <rustamyusupov230@gmail.com>
2022-05-19 17:24:25 +0500

    bug fix

--- app/Http/Controllers/Site/ApplicationController.php
+++ app/Http/Controllers/Site/ApplicationController.php
@@ -176,7 +176,7 @@ class ApplicationController extends Controller
         }
         elseif ($user->hasPermission('Company_Signer') || $user->hasPermission('Add_Company_Signer')||$user->hasPermission('Branch_Signer') || $user->hasPermission('Add_Branch_Signer'))
             {
-            $query = Application::query()->where('signers','like',"%{$user->role_id}%")->orWhere('performer_role_id', $user->role->id)->orWhere('user_id',auth()->user()->id);
+            $query = Application::query()->where('draft','!=',1)->where('signers','like',"%{$user->role_id}%")->orWhere('performer_role_id', $user->role->id)->orWhere('user_id',auth()->user()->id);
         }
         elseif ($user->hasPermission('Company_Performer') || $user->hasPermission('Branch_Performer'))
             {
