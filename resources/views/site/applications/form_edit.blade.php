9e7ea5a7698f30964b549a032bf5ee7d8e57b570
arabic0513 <rustamyusupov230@gmail.com>
2022-05-23 13:51:09 +0500

    bug fix

--- resources/views/site/applications/form_edit.blade.php
+++ resources/views/site/applications/form_edit.blade.php
@@ -328,45 +328,6 @@
 <script src="https://releases.transloadit.com/uppy/v2.4.1/uppy.min.js"></script>
 <script src="https://releases.transloadit.com/uppy/v2.4.1/uppy.legacy.min.js" nomodule></script>
 <script src="https://releases.transloadit.com/uppy/locales/v2.0.5/ru_RU.min.js"></script>
-@if($application->is_more_than_limit != '1' && $application->is_more_than_limit != '0')
-    <input id="is_more_than_limit" value="0" class="hidden">
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script>
-        Swal.fire({
-            title: 'Визирование заявки через компанию',
-            showDenyButton: true,
-            confirmSubmitText: 'Confirm',
-            confirmSubmitClass: 'button is-success has-right-spacing',
-        }).then((result) => {
-            if (result.isConfirmed == true) {
-                document.getElementById('is_more_than_limit').value = 1;
-                ajax();
-            } else if (result.isDenied == true) {
-                document.getElementById('is_more_than_limit').value = 0;
-                ajax();
-            }
-        })
-
-        function ajax() {
-            $.ajax({
-                url: "{{ route('site.applications.update', $application->id) }}",
-                method: "POST",
-                data: {
-                    _token: '{{ csrf_token() }}',
-                    is_more_than_limit: document.getElementById('is_more_than_limit').value,
-                },
-                success: function () {
-                    location.reload();
-                },
-
-                error: function (xhr, ajaxOptions, thrownError) {
-                    console.log(xhr.status);
-                    console.log(thrownError);
-                }
-            })
-        }
-    </script>
-@endif
 <script>
     var uppy = new Uppy.Core({
         debug: true,
