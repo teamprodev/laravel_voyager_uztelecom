854c9e541cbc4ebad5795910665d0e2ba2179503
arabic0513 <rustamyusupov230@gmail.com>
2022-05-25 16:50:59 +0500

    вещь с полосками серыми на главном меню

--- resources/views/site/applications/index.blade.php
+++ resources/views/site/applications/index.blade.php
@@ -7,7 +7,7 @@
         {{ __('lang.create') }}
     </a>
     <div class="w-11/12 mx-auto pt-8 pb-16">
-        <table id="yajra-datatable">
+        <table id="yajra-datatable" class="display wrap">
             <thead  class="text-center">
             <tr>
                 <th>ID</th>
