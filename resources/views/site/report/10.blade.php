bcfe20f1dcf30acfcb2b0749f1be094e15653056
xeroctation <umidakosimova.uk@gmail.com>
2022-05-26 16:53:47 +0500

    report 10 upgraded

--- resources/views/site/report/10.blade.php
+++ resources/views/site/report/10.blade.php
@@ -1,6 +1,6 @@
 @extends('site.layouts.app')
 @section('center_content')
-<!doctype html>
+    <!doctype html>
 <html lang="en">
 <head>
     <link href="https://releases.transloadit.com/uppy/v2.4.1/uppy.min.css" rel="stylesheet">
@@ -21,43 +21,69 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/datetime/1.1.2/css/dataTables.dateTime.min.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.min.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css"/>
+
+    {{--    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/af-2.3.2/b-1.5.4/b-colvis-1.5.4/b-flash-1.5.4/b-html5-1.5.4/b-print-1.5.4/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-1.5.0/sl-1.2.6/datatables.min.css"/>--}}
+
     <style>
         #example_filter{
             display: none;
         }
+        #example_paginate{
+            display: none;
+        }
+        #example_info{
+            display: none;
+        }
+        .dt-buttons{
+            width: 60%;
+            text-align: center;
+            margin-bottom: 15px;
+        }
+        .dataTables_length{
+            width: 20%;
+            margin-bottom: 15px;
+        }
+        .dataTables_filter{
+            width: 20%;
+            margin-bottom: 15px;
+        }
     </style>
 </head>
+
+<div id="fortext"></div>
 {{ Aire::open()
   ->route('request')
   ->enctype("multipart/form-data")
   ->post() }}
-<div style="text-align: end;margin-right: 100px">
+<div style="text-align: center; display: flex; justify-content: end; align-items: center; column-gap: 10px; margin-right: 20px">
     {{Aire::select([2021 => '2021', 2022 => '2022', 2023 => '2023',2024 => '2024'], 'select', 'Год')->value(Illuminate\Support\Facades\Cache::get('date_10'))->name('date_10')}}
-    <button type="submit" class="btn btn-success">Менять</button>
+
+    <button type="submit" class="btn btn-success" style="margin-top: 8px;">Выбрать</button>
 </div>
 {{ Aire::close() }}
 @if(Illuminate\Support\Facades\Cache::get('date_10') != null)
-<table id="example" class="display nowrap" style="width:100%">
-    <thead>
-    <tr>
-        <th>Год</th>
-        <th>январь</th>
-        <th>февраль</th>
-        <th>март</th>
-        <th>апрель</th>
-        <th>май</th>
-        <th>июнь</th>
-        <th>июль</th>
-        <th>август</th>
-        <th>сентябрь</th>
-        <th>октябрь</th>
-        <th>ноябрь</th>
-        <th>декабрь</th>
-        <th>Итого</th>
-    </tr>
-    </thead>
-</table>
+    <table id="example" class="display wrap table-bordered dt-responsive" style="border-collapse: collapse; width: 100%; padding-top: 10px">
+        <thead class="border border-dark">
+        <tr class="border border-dark">
+            <th style="text-align: center;" class="border border-dark">Год</th>
+            <th style="text-align: center;" class="border border-dark">январь</th>
+            <th style="text-align: center;" class="border border-dark">февраль</th>
+            <th style="text-align: center;" class="border border-dark">март</th>
+            <th style="text-align: center;" class="border border-dark">апрель</th>
+            <th style="text-align: center;" class="border border-dark">май</th>
+            <th style="text-align: center;" class="border border-dark">июнь</th>
+            <th style="text-align: center;" class="border border-dark">июль</th>
+            <th style="text-align: center;" class="border border-dark">август</th>
+            <th style="text-align: center;" class="border border-dark">сентябрь</th>
+            <th style="text-align: center;" class="border border-dark">октябрь</th>
+            <th style="text-align: center;" class="border border-dark">ноябрь</th>
+            <th style="text-align: center;" class="border border-dark">декабрь</th>
+            <th style="text-align: center;" class="border border-dark">Итого</th>
+        </tr>
+        </thead>
+    </table>
 @endif
+
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js"></script>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/plug-ins/1.10.21/sorting/datetime-moment.js"></script>
@@ -72,20 +98,30 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.4.1/js/all.js" integrity="sha384-L469/ELG4Bg9sDQbl0hvjMq8pOcqFgkSpwhwnslzvVVGpDjYJ6wJJyYjvG3u8XW7" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/af-2.3.2/b-1.5.4/b-colvis-1.5.4/b-flash-1.5.4/b-html5-1.5.4/b-print-1.5.4/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-1.5.0/sl-1.2.6/datatables.min.js"></script>
+
+
 <script>
     $(document).ready(function() {
-        var buttonCommon = {
-            extend: 'excel',
-            title: '10 отчет по кол-ву статусам',
-            text: '<i title="export to excel" class="fa fa-file-text-o">Excel</i><br>',
-            exportOptions: {
-                columns: ':visible:Not(.not-exported)',
-                rows: ':visible'
-            },
-        };
+        var tableTitle = '10 - отчет по кол-ву статусам';
+
         $('#example').DataTable( {
+            "language": {
+                "lengthMenu": "Показать _MENU_ записей",
+                "info":      'Показаны записи в диапазоне от _START_ до _END_ (В общем _TOTAL_)',
+                "search":  'Поиск',
+                "paginate": {
+                    "previous": "Назад",
+                    "next": "Дальше"
+                }
+
+            },
+            "processing": true,
+            pageLength: 10,
             // dom: 'PQlfrtip',
             dom: 'Qlfrtip' + 'Bfrtip',
+
             ajax:
                 "{{ route('report','10') }}",
 
@@ -105,15 +141,70 @@
                 {data: 'december', name: 'december'},
                 {data: 'all', name: 'all'},
             ],
-            buttons: [
-                $.extend( true, {}, buttonCommon, {
-                    extend: 'excelHtml5'
-                } )
-            ],
+            buttons: {
+                buttons: [
+                    { extend: 'copyHtml5',
+                        text: '<i class="fas fa-copy"></i>',
+                        title: tableTitle,
+                        titleAttr: 'Copy to Clipboard',
+                        exportOptions: {
+                            columns: ':visible:Not(.not-exported)',
+                            rows: ':visible',
+                        },
+                    },
+                    { extend: 'excelHtml5',
+                        text: '<i class="fas fa-file-excel"></i>',
+                        title: tableTitle,
+                        titleAttr: 'Export to Excel',
+                        exportOptions: {
+                            columns: ':visible:Not(.not-exported)',
+                            rows: ':visible',
+                        },
+                    },
+                    { extend: 'pdfHtml5',
+                        text: '<i class="fas fa-file-pdf"></i>',
+                        title: tableTitle,
+                        titleAttr: 'Export to PDF',
+                        orientation: 'landscape',
+                        pageSize: 'LEGAL',
+                        exportOptions: {
+                            columns: ':visible:Not(.not-exported)',
+                            rows: ':visible',
+                        },
+                    },
+                    { extend: 'print',
+                        text: '<i class="fas fa-print"></i>',
+                        title: tableTitle,
+                        titleAttr: 'Print Table',
+                        exportOptions: {
+                            columns: ':visible:Not(.not-exported)',
+                            rows: ':visible',
+                        },
+                    },
+                    { extend: 'colvis',
+                        text: '<i class="fas fa-eye"></i>',
+                        titleAttr: 'Show/Hide Columns',
+                        exportOptions: {
+                            columns: ':visible:Not(.not-exported)',
+                            rows: ':visible',
+                        },
+                    }
+                ],
+                dom: {
+                    button: {
+                        className: 'dt-button'
+                    }
+                }
+            },
+
         });
+        var divTitle = ''
+            + '<div class="col-12 text-center text-md-left pt-4 display-2" style="text-align: center !important;">'
+            + '<h1 class="text-dark">' + tableTitle + '</h1>'
+            + '</div>';
+
+        $("#fortext").append(divTitle);
+
     });
 </script>
-<div class="pl-4 pt-4">
-    <a href="/" class="btn btn-danger">{{ __('lang.back') }}</a>
-</div>
 @endsection
