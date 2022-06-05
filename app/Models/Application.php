2ba2bdc24641128c7aa09257032dbdb3a215c132
arabic0513 <rustamyusupov230@gmail.com>
2022-06-05 15:04:52 +0500

    phpdocumentor & soft delete customized

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -5,12 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
-use phpDocumentor\Reflection\Types\Resource_;
+use phpDocumentor\Reflection\Types\Resource;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Application extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
+    protected $dates = ['deleted_at'];
     const NEW = 'new';
     const IN_PROCESS = 'in_process';
     const CANCELED = 'canceled';
