9ea166dc87f2bc809012e10fc49dfd55f413e7b2
arabic0513 <rustamyusupov230@gmail.com>
2022-08-05 19:51:01 +0500

    bug fix e-imzo.uz

--- resources/views/site/auth/auth.blade.php
+++ resources/views/site/auth/auth.blade.php
@@ -44,6 +44,8 @@
 
 </div>
 
+<script src="{{ asset('assets/js/eimzo/e-imzo.js') }}"></script>
+<script src="{{ asset('assets/js/eimzo/e-imzo-client.js') }}"></script>
 <script src="{{ asset('assets/js/eimzo/imzo.js') }}"></script>
 </body>
 </html>
