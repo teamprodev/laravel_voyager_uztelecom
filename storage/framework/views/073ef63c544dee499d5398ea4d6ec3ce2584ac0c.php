a507cabb7be86c48c56cf98df67dc7933fbaf292
arabic0513 <rustamyusupov230@gmail.com>
2022-08-22 11:22:07 +0500

    133333333333

---
+++ storage/framework/views/073ef63c544dee499d5398ea4d6ec3ce2584ac0c.php
@@ -0,0 +1,4 @@
+<?php /** @var \Galahad\Aire\Elements\Attributes\Collection $attributes */ ?>
+
+<input <?php echo e($attributes); ?> />
+<?php /**PATH D:\Develop\Panels\OpenServer\domains\laravel_voyager_uztelecom\vendor\glhd\aire/views/input.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/dd540c944cb13b6b67da5d507388799937021323.php
@@ -0,0 +1,7 @@
+<?php /** @var \Galahad\Aire\Elements\Attributes\Collection $attributes */ ?>
+
+<label <?php echo e($attributes); ?>>
+	<?php echo e($text ?? ''); ?>
+
+</label>
+<?php /**PATH D:\Develop\Panels\OpenServer\domains\laravel_voyager_uztelecom\vendor\glhd\aire/views/label.blade.php ENDPATH**/ ?>
