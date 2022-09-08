e1eba7109cff4ef5cc6f54df1ad785d9cd425615
xeroctation <umidakosimova.uk@gmail.com>
2022-09-08 11:06:32 +0500

    fixed someone's bug submit empty branch_id

--- app/Jobs/UpdateProfileJob.php
+++ app/Jobs/UpdateProfileJob.php
@@ -39,7 +39,6 @@ class UpdateProfileJob implements ShouldQueue
         DB::beginTransaction();
         try {
             $user = User::find(Auth::id());
-            $user->branch_id = $this->profileRequest->fillial;
             $user->phone = $this->profileRequest->phone;
             $user->email = $this->profileRequest->email;
             $user->save();
