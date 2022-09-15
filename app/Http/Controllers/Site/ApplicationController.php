b7857e03b5509b1c5012d983e0c25d777582a6c9
arabic0513 <rustamyusupov230@gmail.com>
2022-09-15 17:49:48 +0500

    bug fix

--- app/Http/Controllers/Site/ApplicationController.php
+++ app/Http/Controllers/Site/ApplicationController.php
@@ -121,7 +121,7 @@ class ApplicationController extends Controller
     */
     public function edit(Application $application)
     {
-        if($application->status == 'distributed' || $application->show_leader == 2)
+        if($application->status == 'distributed' && $application->performer_role_id != auth()->user()->role_id || $application->show_leader == 2 && $application->performer_role_id != auth()->user()->role_id)
         {
             abort(405,"Вам нельзя изменить заявку,ибо заявка уже распределена!");
         }
