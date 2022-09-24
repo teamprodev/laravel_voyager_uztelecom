3c22f858c0f575449037015348b7c104d80a5301
arabic0513 <rustamyusupov230@gmail.com>
2022-09-24 15:13:08 +0500

    code refactoring & magic numbers in ROleCOntroller

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use TCG\Voyager\Models\Role;
 
+/**
+ * @method static find(mixed $all)
+ */
 class Branch extends Model
 {
     use HasFactory;
