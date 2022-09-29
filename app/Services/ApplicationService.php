50ed9b55d415e15473fb0a7c0ac38faae00c8e8c
arabic0513 <rustamyusupov230@gmail.com>
2022-09-29 17:36:31 +0500

    bug fixx

--- app/Services/ApplicationService.php
+++ app/Services/ApplicationService.php
@@ -140,7 +140,7 @@ class ApplicationService
                     switch($status)
                     {
                         case $query->performer_status !== null:
-                            $a = StatusExtented::find($query->performer_status)->first();
+                            $a = StatusExtented::find($query->performer_status);
                             return $this->status($a->name);
                         case ApplicationData::Status_New:
                             $status = setting('color.new');
@@ -250,7 +250,7 @@ class ApplicationService
                 $d = [$user->department_id];
             }
         $status = setting('admin.show_status');
-        $data = Application::whereIn($a, $b)->where('status', $status)->where('name', '!=', null)->get();
+        $data = Application::whereIn($a, $b)->where('performer_status', $status)->where('name', '!=', null)->get();
         return Datatables::of($data)
             ->addIndexColumn()
             ->editColumn('user_id', function ($docs) {
@@ -278,7 +278,7 @@ class ApplicationService
                 switch($status)
                 {
                     case $query->performer_status !== null:
-                        $a = StatusExtented::find($query->performer_status)->first();
+                        $a = StatusExtented::find($query->performer_status);
                         return $this->status($a->name);
                     case ApplicationData::Status_New:
                         $status = setting('color.new');
@@ -406,7 +406,7 @@ class ApplicationService
                 switch($status)
                 {
                     case $query->performer_status !== null:
-                        $a = StatusExtented::find($query->performer_status)->first();
+                        $a = StatusExtented::find($query->performer_status);
                         return $this->status($a->name);
                     case ApplicationData::Status_New:
                         $status = setting('color.new');
@@ -954,7 +954,7 @@ class ApplicationService
                 switch($status)
                 {
                     case $query->performer_status !== null:
-                        $a = StatusExtented::find($query->performer_status)->first();
+                        $a = StatusExtented::find($query->performer_status);
                         return $this->status($a->name);
                     case ApplicationData::Status_New:
                         $status = setting('color.new');
@@ -1048,34 +1048,44 @@ class ApplicationService
             case 'Принята':
                 $status = setting('color.accepted');
                 $color = $status ? 'white' : 'black';
-                return "<div style='background-color: {$status};color: {$color};' class='text-center m-1 col edit btn-sm'>{$status_accepted}</div>";
+                $return_status = "<div style='background-color: {$status};color: {$color};' class='btn btn-sm'>{$status_accepted}</div>";
+                break;
             case 'Выполнено частично':
                 $status = setting('color.partially');
                 $color = $status ? 'white' : 'black';
-                return "<div style='background-color: {$status};color: {$color};' class='text-center m-1 col edit btn-sm'>Выполнено частично</div>";
+                $return_status = "<div style='background-color: {$status};color: {$color};' class='btn btn-sm'>Выполнено частично</div>";
+                break;
             case 'Выполнено в полном объёме':
                 $status = setting('color.total_volume');
                 $color = $status ? 'white' : 'black';
-                return "<div style='background-color: {$status};color: {$color};' class='text-center m-1 col edit btn-sm'>Выполнено в полном объёме</div>";
+                $return_status = "<div style='background-color: {$status};color: {$color};' class='btn btn-sm'>Выполнено в полном объёме</div>";
+                break;
             case 'Заявка аннулирована по заданию руководства':
                 $status = setting('color.nulled_by_management');
                 $color = $status ? 'white' : 'black';
-                return "<div style='background-color: {$status};color: {$color};' class='text-center m-1 col edit btn-sm'>Заявка аннулирована по заданию руководства</div>";
+                $return_status = "<div style='background-color: {$status};color: {$color};' class='btn btn-sm'>Заявка аннулирована по заданию руководства</div>";
+                break;
             case 'Договор аннулирован по инициативе Узбектелеком':
                 $status = setting('color.nulled_by_management');
                 $color = $status ? 'white' : 'black';
-                return "<div style='background-color: {$status};color: {$color};' class='text-center m-1 col edit btn-sm'>Договор аннулирован по инициативе Узбектелеком</div>";
+                $return_status = "<div style='background-color: {$status};color: {$color};' class='btn btn-sm'>Договор аннулирован по инициативе Узбектелеком</div>";
+                break;
             case 'заявка передана в Узтелеком':
                 $status = setting('color.nulled_by_management');
                 $color = $status ? 'white' : 'black';
-                return "<div style='background-color: {$status};color: {$color};' class='text-center m-1 col edit btn-sm'>заявка передана в Узтелеком</div>";
+                $return_status = "<div style='background-color: {$status};color: {$color};' class='btn btn-sm'>заявка передана в Узтелеком</div>";
+                break;
             case 'товар доставлен':
                 $status = setting('color.delivered');
                 $color = $status ? 'white' : 'black';
-                return "<div class='row'>
-                            <div style='background-color: {$status};color: {$color};' class='text-center m-1 col edit btn-sm'>{$status_performed}</div>
+                $return_status = "<div class='row'>
+                            <div style='background-color: {$status};color: {$color};' class='btn btn-sm'>{$status_performed}</div>
                             </div>";
+                break;
+            default:
+                $return_status = $status;
         }
+        return $return_status;
     }
     public function status_1(string $status)
     {
