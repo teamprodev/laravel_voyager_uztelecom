a3ca572ba0708a8166fd045b841f6893db360338
Bexruz999 <bexruzfatullayev999@gmail.com>
2022-10-01 23:43:46 +0500

    bug fix

--- resources/views/site/applications/crud_link.blade.php
+++ resources/views/site/applications/crud_link.blade.php
@@ -1,6 +1,6 @@
 <div class='row'>
     @foreach($data as $key => $elem)
-    <a style='background-color: {{setting("color.{$key}")}};color: {{setting('color.' . _($key)) ? 'white' : 'black'}}'
-       href='{{$elem}}' class='m-1 col edit btn btn-sm' {{$key === 'destroy' ? "onclick='return confirm(`$confirm`)'" : ''}}>{{$key}}</a>
+    <a style='background-color: {{setting("color.{$key}")}};color: {{setting('color.' . $key) ? 'white' : 'black'}}'
+       href='{{$elem}}' class='m-1 col edit btn btn-sm' {{$key === 'destroy' ? "onclick='return confirm(`$confirm`)'" : ''}}>{{__($key)}}</a>
     @endforeach
 </div>
