0fcd7afe576f412b6238b7d8e2d10e16130174c5
Bexruz999 <bexruzfatullayev999@gmail.com>
2022-10-01 23:35:52 +0500

    texnik nosozlik

--- resources/views/site/applications/crud_link.blade.php
+++ resources/views/site/applications/crud_link.blade.php
@@ -1,6 +1,6 @@
 <div class='row'>
     @foreach($data as $key => $elem)
     <a style='background-color: {{setting("color.{$key}")}};color: {{setting('color.' . _($key)) ? 'white' : 'black'}}'
-       href='{{$elem}}' class='m-1 col edit btn btn-sm' {{$key === 'destroy' ? "onclick='return confirm(`$confirm`)'" : ''}}>$key</a>
+       href='{{$elem}}' class='m-1 col edit btn btn-sm' {{$key === 'destroy' ? "onclick='return confirm(`$confirm`)'" : ''}}>{{$key}}</a>
     @endforeach
 </div>
