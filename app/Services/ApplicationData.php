81c6581441a08972f90ff5bf495ffc412d684b48
MIXA8 <javonsher@list.ru>
2022-10-02 14:55:43 +0500

    Revert "Revert "Create ApplicationData.php""
    This reverts commit 2813e095559fd2098d3392d4cc98b7b1659d33b7.

---
+++ app/Services/ApplicationData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+
+class ApplicationData
+{
+    public const Status_New = 'new';
+    public const Status_In_Process = 'in_process';
+    public const Status_Extended = 'extended';
+    public const Status_Overdue = 'overdue';
+    public const Status_Accepted = 'accepted';
+    public const Status_Refused = 'refused';
+    public const Status_Agreed = 'agreed';
+    public const Status_Rejected = 'rejected';
+    public const Status_Distributed = 'distributed';
+    public const Status_Canceled = 'canceled';
+    public const Status_Partially_Completed = 'partially_completed';
+    public const Status_Completed_Full = 'completed_full';
+    public const Status_Management_Canceled  = 'management_canceled';
+    public const Status_Uztelecom_Canceled = 'uztelecom_canceled';
+    public const Status_Application_Uztelecom = 'application_uztelecom';
+    public const Status_Order_Delivered = 'order_delivered';
+    public const Status_Order_Arrived = 'order_arrived';
+    public const Status_Contract_Concluded = 'contract_concluded';
+    public const Status_Draft = 'draft';
+
+
+
+}
