eb5459126f574e8b7cb3313ed2f70635e8082276
MIXA8 <58101328+MIXA8@users.noreply.github.com>
2022-10-24 16:59:34 +0500

    write codeseption test
    -create
    -edit
    -destroy

--- tests/acceptance/ApplicationCest.php
+++ tests/acceptance/ApplicationCest.php
@@ -20,52 +20,47 @@ class ApplicationCest
         $I->click('Создать');
         $I->click('Компания');
         $I->amOnPage($I->grabFromCurrentUrl());
-        $I->fillField("//*[@name='initiator']", 'TEST!!! Тошкент омборидан Республика омборхоналаригача (ёки обьектларгачам)');
-        $I->fillField("textarea[name='purchase_basis']", 'TEST!!!  “Ўзбектелеком” АК томонидан имзоланган шартномаларга асосан келтириладиган телекоммуникация қурилмаларини ҳамда  “ЎзМобайл” филиали томонидан харид қилинган электр, қурилиш хўжалик ва бошқа турдаги маҳсулотларини Республика ҳудудий омборхоналарига ва обьектларга етказиб бериш.');
-        $I->fillField("textarea[name='basis']", 'TEST!!!  Бизнес режа асосида.');
-        $I->fillField("input[name='name']", '111ртнома тузиш.');
-        $I->fillField("textarea[name='specification']", 'TEST!!! Тент, контейнеровоз.');
-        $I->fillField("textarea[name='separate_requirements']", 'TEST!!! Тент, контейнеровоз.');
-        $I->fillField("textarea[name='other_requirements']", 'TEST!!! Тент, контейнеровоз.');
-        $I->fillField("input[name='planned_price']", '550000000');
-        $I->fillField("textarea[name='incoterms']", 'TEST!!!  Тошкент омборидан Республика омборхоналаригача (ёки обьектларгачам)');
-        $I->fillField("input[name='info_business_plan']", 'TEST!!! САРЕХ 2022');
-        $I->fillField("textarea[name='info_purchase_plan']", 'TEST!!! Планом закупок предусмотрено');
-        $I->fillField("textarea[name='comment']", 'java adsada');
-        $I->selectOption("//form//select[@name='currency']", "USD");
-//        $I->selectOption("//form//table//select", "21");
-        $I->click('Сохранить и отправить');
-
+        $I->submitForm("//form[@data-aire-component='form']", [
+            'initiator' => 'TEST!!! Тошкент омборидан Республика омборхоналаригача (ёки обьектларгачам)',
+            'basis' => 'TEST!!!  “Ўзбектелеком” АК томонидан имзоланган шартномаларга асосан келтириладиган телекоммуникация қурилмаларини ҳамда',
+            'name' => 'JAVATEST!!!  Бизнес режа асосида.',
+            'specification' => 'TEST!!!  Бизнес режа асосида.',
+            'separate_requirements' => 'TEST!!! Тент, контейнеровоз.',
+            'other_requirements' => 'TEST!!! Тент, контейнеровоз.',
+            'planned_price' => '550000000',
+            'incoterms' => 'TEST!!!  Тошкент омборидан Республика омборхоналаригача (ёки обьектларгачам)',
+            'info_business_plan' => 'TEST!!! САРЕХ 2022',
+            'info_purchase_plan' => 'TEST!!! Планом закупок предусмотрено',
+            'comment' => 'java adsada',
+            'resource_id' => 22,
+            'currency' => 'USD'
+        ], 'draft');
         $this->id = (int)$this->grabFromUrlIdApplication($I->grabFromCurrentUrl());
-
-
     }
 
     public function edit(AcceptanceTester $I)
     {
-
         $I->am('admin');
         $I->amOnPage('/admin/login');
         $I->fillField('email', 'aziz@gmail.com');
         $I->fillField('password', 'password');
         $I->click('Войти');
-//        $I->amOnUrl("http://127.0.0.1:8000/ru/site/applications/$this->id/edit");
-//        var_dump($I->grabFromCurrentUrl());
-//        $I->fillField(["//*[@name='initiator']"], 'TEST!!! Бухара омборидан Республика омборхоналаригача (ёки обьектларгачам)');
-//        $I->fillField("textarea[name='purchase_basis']", 'TEST!!!  “Бухара” АК томонидан имзоланган шартномаларга асосан келтириладиган телекоммуникация қурилмаларини ҳамда  “ЎзМобайл” филиали томонидан харид қилинган электр, қурилиш хўжалик ва бошқа турдаги маҳсулотларини Республика ҳудудий омборхоналарига ва обьектларга етказиб бериш.');
-//        $I->fillField("textarea[name='basis']", 'TEST!!!  Бухара Бухара Бухара.');
-//        $I->fillField("input[name='name']", 'TEST!!!  Бухара.');
-//        $I->fillField("textarea[name='specification']", 'TEST!!! Бухара.');
-//        $I->fillField("textarea[name='separate_requirements']", 'TEST!!! Бухара.');
-//        $I->fillField("textarea[name='other_requirements']", 'TEST!!! Бухара, Бухара.');
-//        $I->fillField("input[name='planned_price']", '560000000');
-//        $I->fillField("textarea[name='incoterms']", 'TEST!!!  Бухара ');
-//        $I->fillField("input[name='info_business_plan']", 'TEST!!! Бухара 2022');
-//        $I->fillField("textarea[name='info_purchase_plan']", 'TEST!!! Бухара');
-//        $I->fillField("textarea[name='comment']", 'java adsada');
-//        $I->grabValueFrom(['name' => 'info_business_plan']);
-//        $I->checkOption("[name='with_nds']");
-//        $I->click('Сохранить и отправить');
+        $I->amOnUrl("http://127.0.0.1:8000/ru/site/applications/$this->id/edit");
+        $I->submitForm("//form[@data-aire-id='0']", [
+            'initiator' => 'TEST!!! Тошкент омборидан Республика омборхоналаригача (ёки обьектларгачам)',
+            'basis' => 'TEST!!!  “Ўзбектелеком” АК томонидан имзоланган шартномаларга асосан келтириладиган телекоммуникация қурилмаларини ҳамда',
+            'name' => 'JAVATEST перестест!!!  Бизнес режа асосида.',
+            'specification' => 'TEST!!!  Бизнес режа асосида.',
+            'separate_requirements' => 'TEST!!! Тент, контейнеровоз.',
+            'other_requirements' => 'TEST!!! Тент, контейнеровоз.',
+            'planned_price' => '550000000',
+            'incoterms' => 'TEST!!!  Тошкент омборидан Республика омборхоналаригача (ёки обьектларгачам)',
+            'info_business_plan' => 'TEST!!! САРЕХ 2022',
+            'info_purchase_plan' => 'TEST!!! Планом закупок предусмотрено',
+            'comment' => 'java adsada',
+            'resource_id' => 32,
+            'currency' => 'USD'
+        ], "//form[@data-aire-component='form']//button[@value=0]");
     }
 
     public function clone(AcceptanceTester $I)
