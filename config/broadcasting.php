f749e4934dc6361abfbea2928bcee7889a9a5184
Faxriddin077 <faxriddinqodirov077@gmail.com>
2022-10-30 11:39:43 +0500

    pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,8 +39,11 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => env('LARAVEL_WEBSOCKETS_HOST', '127.0.0.1'),
                 'port' => env('LARAVEL_WEBSOCKETS_PORT', 6001),
-                'scheme' => env('LARAVEL_WEBSOCKETS_SCHEME', 'http'),
-
+                'scheme' => 'https',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ],
                 'encrypted' => true,
                 'forceTLS' => true,
                 'disableStats' => true,
