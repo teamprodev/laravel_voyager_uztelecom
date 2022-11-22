af34cce762b1626093ee18dd4f0880e20b8482b2
arabic0513 <rustamyusupov230@gmail.com>
2022-11-22 14:02:10 +0500

    added method static where

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static where(string $string, string $string1)
+ */
 class Setting extends Model
 {
     use HasFactory;
