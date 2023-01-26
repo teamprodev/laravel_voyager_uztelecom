87d1ee5dd384ecf1a3540c4503ecb55df0e2d67f
arabic0513 <rustamyusupov230@gmail.com>
2023-01-26 14:40:04 +0500

    deleted timezone Asia/Tashkent

--- public/vendor/eimzo/assets/js/eimzo/imzo.js
+++ public/vendor/eimzo/assets/js/eimzo/imzo.js
@@ -119,18 +119,7 @@ var uiLoaded = function(){
 }
 
 var uiCreateItem = function (itmkey, vo) {
-    var now;
-    $.ajax({
-        type: 'GET', //THIS NEEDS TO BE GET
-        url: 'https://worldtimeapi.org/api/timezone/Asia/Tashkent',
-        dataType: 'json',
-        async: false,
-        success: function (value) {
-            now = value.datetime;
-        }, error: function () {
-            console.log('error');
-        }
-    });
+    var now = new Date();
     vo.expired = dates.compare(now, vo.validTo) > 0;
     var itm = document.createElement("option");
     itm.value = itmkey;
