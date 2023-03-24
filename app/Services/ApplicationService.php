51ec1eb0076dc4109001c3431e28943432735ea9
arabic0513 <rustamyusupov230@gmail.com>
2023-03-24 16:41:06 +0500

    notification send

--- app/Services/ApplicationService.php
+++ app/Services/ApplicationService.php
@@ -866,7 +866,7 @@ class ApplicationService
     public function sendNotifications($array, $application, $message)
     {
         if ($array !== null) {
-            $user_ids = User::query()->whereIn('role_id', $array)->where('branch_id', $application->branch_initiator_id)->pluck('id')->toArray();
+            $user_ids = User::query()->whereIn('role_id', $array)->pluck('id')->toArray();
             foreach ($user_ids as $user_id) {
                 $notification = Notification::query()->firstOrCreate(['user_id' => $user_id, 'application_id' => $application->id, 'message' => $message]);
                 if ($notification->wasRecentlyCreated) {
