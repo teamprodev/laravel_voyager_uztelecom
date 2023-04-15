296b2400c95521183d1bdf2796cf34a822ae650d
arabic0513 <rustamyusupov230@gmail.com>
2023-04-15 20:11:42 +0500

    report component package

--- resources/views/site/report/_6.blade.php
+++ resources/views/site/report/_6.blade.php
@@ -2,5 +2,5 @@
 @extends('site.layouts.report')
 @section('center_content')
 <x-laravelDateRangePicker format="YYYY-MM-DD" reportId="6" route="{{ route('site.report.index','6') }}"/>
-<x-laravelYajraL tableId="report6" stateSave="true" :dtColumns=$dtColumns :dtHeaders=$dtHeaders dom='Blfrtip' getData="{{ route('report','6') }}" exportId="{{ route('report_export','6') }}" tableTitle="{{__('6 - Отчет свод')}}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"/>
+<x-laravelYajra tableId="report6" serverSide="true" stateSave="true" :dtColumns=$dtColumns :dtHeaders=$dtHeaders dom='Blfrtip' getData="{{ route('report','6') }}" exportId="{{ route('report_export','6') }}" tableTitle="{{__('6 - Отчет свод')}}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"/>
 @endsection
