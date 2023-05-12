9d9f1f44df3eeb0944c0034b73132e23ebc49377
arabic0513 <rustamyusupov230@gmail.com>
2023-05-12 15:57:44 +0500

    fixed route url

--- app/Services/DepartmentService.php
+++ app/Services/DepartmentService.php
@@ -34,40 +34,24 @@ class DepartmentService
     private function createBlockAction($data, $row): string
     {
         $block = '';
-        if (!empty($data['show'])) {
-            $block .= $this->getLinkHtmlBladeShow($row);
-        }
         if (!empty($data['edit'])) {
             $block .= "</br>" . $this->getLinkHtmlBladeEdit($row);
         }
         if (!empty($data['destroy'])) {
             $block .= "</br>" . $this->getLinkHtmlBladeDestroy($row);
         }
-        if (!empty($data['clone'])) {
-            $block .= "</br>" . $this->getLinkHtmlBladeClone($row);
-        }
         return $block;
     }
 
     private function getLinkHtmlBladeEdit($row)
     {
-        return "<a href='" . route("site.applications.edit", $row->id) . "' class='m-1 col edit btn btn-sm btn-secondary'> " . __('edit') . "</a>";
-    }
-
-    private function getLinkHtmlBladeShow($row)
-    {
-        return "<a style='background-color: #000080; color: white' href='" . route('site.applications.show', $row->id) . "' class='m-1 col edit btn btn-sm'> " . __('show') . " </a>";
+        return "<a href='/admin/departments/{$row->id}/edit' class='m-1 col edit btn btn-sm btn-secondary'> " . __('edit') . "</a>";
     }
 
     private function getLinkHtmlBladeDestroy($row)
     {
         $alert_word = __('Вы уверены?');
         $alert = "onclick='return confirm(`{$alert_word}`)'";
-        return "<a href='" . route('site.applications.destroy', $row->id) . "' ${alert} class='m-1 col edit btn btn-sm btn-danger' > " . __('destroy') . " </a>";
-    }
-
-    private function getLinkHtmlBladeClone($row)
-    {
-        return "<a href='" . route('site.applications.clone', $row->id) . "' class='m-1 col edit btn btn-sm btn-secondary'> " . __('edit') . "</a>";
+        return "<a href='" . route("voyager.departments.destroy", $row->id) . "' ${alert} class='m-1 col edit btn btn-sm btn-danger' > " . __('destroy') . " </a>";
     }
 }
