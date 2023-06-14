93646f52d8474feb049735fea63fbf173de07312
arabic0513 <rustamyusupov230@gmail.com>
2023-06-14 15:38:17 +0500

    report 4 deleted tableTitle

--- resources/views/site/report/4.blade.php
+++ resources/views/site/report/4.blade.php
@@ -1,5 +1,5 @@
 @extends('site.layouts.app')
 @section('center_content')
 <x-laravelDateRangePicker format="YYYY-MM-DD" reportId="4" route="{{ route('site.report.index','4') }}"/>
-<x-laravelYajra language="ru" tableId="report4" dom='QBlfrtip' serverSide="true" getData="{{ route('report','4') }}" exportId="{{\App\Reports\Four::class}}" tableTitle="{{ __('4 - Отчет заявки по статусам') }}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"/>
+<x-laravelYajra language="ru" tableId="report4" dom='QBlfrtip' serverSide="true" getData="{{ route('report','4') }}" exportId="{{\App\Reports\Four::class}}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"/>
 @endsection
