5443a1a1731f57cf8a39623fb4404a6ccddd4bc0
arabic0513 <rustamyusupov230@gmail.com>
2023-06-14 15:40:20 +0500

    report 10 deleted tableTitle

--- resources/views/site/report/10.blade.php
+++ resources/views/site/report/10.blade.php
@@ -2,5 +2,5 @@
 @section('center_content')
 
 <x-laravelDateRangePicker format="YYYY" reportId="10" route="{{ route('site.report.index','10') }}"/>
-<x-laravelYajra language="ru" tableId="report10" scrollX="true" dom='QBlfrtip' serverSide="true" getData="{{ route('report','10') }}" exportId="{{ \App\Reports\Ten::class }}" tableTitle="{{__('10 - Отчет по кол-ву статусам')}}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"/>
+<x-laravelYajra language="ru" tableId="report10" scrollX="true" dom='QBlfrtip' serverSide="true" getData="{{ route('report','10') }}" exportId="{{ \App\Reports\Ten::class }}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"/>
 @endsection
