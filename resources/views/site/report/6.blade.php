ff127d1da062ce54b75a737a414f549fd8dee476
arabic0513 <rustamyusupov230@gmail.com>
2023-06-15 11:04:20 +0500

    updated report 6

--- resources/views/site/report/6.blade.php
+++ resources/views/site/report/6.blade.php
@@ -1,5 +1,5 @@
 @extends('site.layouts.app')
 @section('center_content')
 <x-laravelDateRangePicker format="YYYY-MM-DD" reportId="6" route="{{ route('site.report.index','6') }}"/>
-<x-laravelYajra language="ru" tableId="report6" stateSave="true" dom='QBlfrtip' serverSide="true" getData="{{ route('report','6') }}" exportId="{{ \App\Reports\Six::class }}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"/>
+<x-laravelYajra language="ru" tableId="report6" stateSave=true dom='QBlfrtip' serverSide=true getData="{{ route('report','6') }}" exportId="{{ \App\Reports\Six::class }}" startDate="{{request()->input('startDate')}}" endDate="{{request()->input('endDate')}}"/>
 @endsection
