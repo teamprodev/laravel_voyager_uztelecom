5b5d2dcbcdb259c1845eca0aa1b6d1d3c146c0fc
arabic0513 <rustamyusupov230@gmail.com>
2023-06-15 14:01:40 +0500

    Setting doc

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 
 /**
- * @method static where(string $string, string $string1)
+ * @method static Builder where($column, $operator = null, $value = null, $boolean = 'and')
  */
 class Setting extends Model
 {
